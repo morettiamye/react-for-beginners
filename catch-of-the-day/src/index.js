9f5542e2fc62cc5440fd994862c68511757c36a8
Amy <amyemoretti@gmail.com>
2019-02-12 17:02:35 -0600

    Videos 1-6 complete

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from "react";
+import {render} from "react-dom";
+import "./css/style.css";
+
+// Components
+import App from "./components/App"
+
+render(<App />, document.querySelector("#main"));
