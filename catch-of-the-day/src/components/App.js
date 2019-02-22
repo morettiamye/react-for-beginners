b4bd9d98526b4ff1ac28f46821d0f4b1d0b662cc
Amy <amyemoretti@gmail.com>
2019-02-22 13:27:52 -0600

    Bug squashed in app/js

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -6,36 +6,54 @@ import sampleFishes from "../sample-fishes"
 import Fish from "./Fish";
 
 
-class App extends React.Component{
+class App extends React.Component {
     state = {
         fishes: {},
         order: {}
     }
 
     addFish = fish => {
-       const fishes = {...this.state.fishes};
+        const fishes = { ...this.state.fishes };
         fishes[`fish${Date.now()}`] = fish;
         this.setState({
             fishes: fishes
-        })        
+        })
     }
 
     loadSamples = () => {
-        // console.log("This fish tho!");
-        this.setState({fishes: sampleFishes});
+        this.setState({ fishes: sampleFishes });
     }
 
-    render(){
+    addToOrder = key => {
+        const order = { ...this.state.order }
+        order[key] = order[key] + 1 || 1;
+        this.setState({ order: order })
+    }
+
+    render() {
         return (
             <div className="catch-of-the-day">
-            <div className="menu">
-                <Header tagline="Fresh Seafood Market"/>
-                <ul className="fishes">
-                {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/> )}
-                </ul>
-            </div>
-            <Order />
-            <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+                <div className="menu">
+                    <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key =>
+                            <Fish
+                                key={key}
+                                index={key}
+                                details={this.state.fishes[key]}
+                                addToOrder={this.addToOrder}
+                            />
+                        )}
+                    </ul>
+                </div>
+                <Order 
+                    fishes={this.state.fishes} 
+                    order={this.state.order}
+                    />
+                <Inventory 
+                    addFish={this.addFish} 
+                    loadSamples={this.loadSamples} 
+                />
             </div>
         )
     }
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -4,7 +4,11 @@ class Order extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <div className="order">
+                <div className="order-wrap">
+                <h2>Order</h2>
+                <ul>
+                    
+                </ul>
                 </div>
             </React.Fragment>
         )
