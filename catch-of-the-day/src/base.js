82457e0ca1b000a25d296ab780fc7d4d3e1a4312
Amy <amyemoretti@gmail.com>
2019-02-23 14:43:10 -0600

    Saving order state to local storage, also corrected Order/JS bug

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -10,7 +10,7 @@ const firebaseApp =  firebase.initializeApp({
 
 const base = Rebase.createClass(firebaseApp.database());
 
-// This is a named export
+
 export {firebaseApp};
 
 export default base;
