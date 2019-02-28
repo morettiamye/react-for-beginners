45f56bfd244558e15bda084e0334d7538eef037c
Amy <amyemoretti@gmail.com>
2019-02-28 16:24:39 -0600

    another smack

--- catch-of-the-day/src/components/LogIn.js
+++ catch-of-the-day/src/components/LogIn.js
@@ -2,13 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const LogIn = props => (
+    <React.Fragment>
   <nav className="login">
     <h2>Inventory Login</h2>
-    <p>Sign in to manage your store's inventory.</p>
+    <p>Sign in to manage your stores inventory.</p>
     <button className="github" onClick={() => props.authenticate("Github")}>
       Log In With GitHub
     </button>
   </nav>
+  </React.Fragment>
 );
 
 LogIn.propTypes = {
